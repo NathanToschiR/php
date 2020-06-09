f2ea72366985c75317ff684a2d7eee76a64fb4f3
Nathan Toschi <nathan.toschi@ice.ufjf.br>
2020-06-08 22:54:34 -0300

    footer e head arrumados

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,2 +1,7 @@
+
+<script src="../../../public/js/jquery-3.5.1.slim.min.js" ></script>
+<script src="../../../public/js/popper.min.js"></script>
+<script src="../../../public/js/bootstrap.min.js" ></script>	
+	
 </body>
 </html>
